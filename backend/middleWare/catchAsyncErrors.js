4e51969caaf4c517fb8ac02770575d3d9779e41e
Maneesh  Mondal <maneeshmondal56@gmail.com>
2023-01-28 13:39:29 +0530

    Added Files

--- backend/middleWare/catchAsyncErrors.js
+++ backend/middleWare/catchAsyncErrors.js
@@ -1,3 +1,5 @@
-module.exports = (theFunc) => (req, res, next) => {
+const catchAsyncErrors = (theFunc) => (req, res, next) => {
     Promise.resolve(theFunc(req, res, next)).catch(next);
   };
+
+  module.exports = catchAsyncErrors
