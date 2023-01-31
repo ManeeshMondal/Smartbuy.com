6dc5588ab0e8f912ded730859ff0a7fb6344cb86
Maneesh  Mondal <maneeshmondal56@gmail.com>
2023-01-31 20:24:48 +0530

    pushing

--- backend/app.js
+++ backend/app.js
@@ -23,6 +23,13 @@ const order=require("./routes/orderRoute")
 
 const cors = require('cors');
 app.use(cors());
+app.use(function (req, res, next) {
+    //Enabling CORS
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+      next();
+    });
 // routes 
 app.use("/api/v1",product);
 app.use("/api/v1",user)
