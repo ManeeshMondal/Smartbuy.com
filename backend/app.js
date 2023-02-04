37c2b3edcc9f03a38241e883af2c7f3703bef408
Maneesh  Mondal <maneeshmondal56@gmail.com>
2023-02-05 00:27:46 +0530

    updates

--- backend/app.js
+++ backend/app.js
@@ -18,6 +18,11 @@ var cors = require('cors');
 // }
 app.use(cors());
 
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+  });
+
 // app.use(function(req, res, next) {
 //     res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
 //     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
